7e4d577a0c2cdd7fbe6e3bbc4a2ab7a26245d2b1
SunDapeng <18333158373@163.com>
2020-02-28 18:36:49 +0800

    临时添加GPIO模块

--- PlatformNto1.h
+++ PlatformNto1.h
@@ -23,4 +23,56 @@ extern unsigned int LibVersion(void);
 
 
 
+/************************************************************************************************************/
+//GPIO
+/************************************************************************************************************/
+/* GPIO pins */
+#define	GPIO_0					(0)
+#define	GPIO_1					(1)
+#define	GPIO_2					(2)
+#define	GPIO_3					(3)
+#define	GPIO_4					(4)
+#define	GPIO_5					(5)
+#define	GPIO_6					(6)
+#define	GPIO_7					(7)
+#define	GPIO_8					(8)
+#define	GPIO_9					(9)
+#define	GPIO_10					(10)
+#define	GPIO_11					(11)
+#define	GPIO_12					(12)
+#define	GPIO_13					(13)
+#define	GPIO_14					(14)
+#define	GPIO_15					(15)
+#define	GPIO_MAX_NUM			GPIO_15
+/*****************************************************************************
+ Prototype    : GPIO_init
+ Description  : GPIO初始化
+ Input        : unsigned short Dir,采用位图表示IO方向，LSB为GPIO0；0为输出，1为输入；初始化默认输出值为0。
+ 	 	 	 	unsigned char IntEn, 中断使能，1使能，0关闭
+ 	 	 	 	unsigned char TrigMode, 中断触发方式，0，上升沿，1下降沿
+ Output       : 无
+ Return Value : 无
+*****************************************************************************/
+extern void GPIO_init(unsigned short Dir, unsigned char IntEn, unsigned char TrigMode);
+/*****************************************************************************
+ Prototype    : Set_GPIO
+ Description  : GPIO端口置1
+ Input        : unsigned char num，GPIO端口号。
+ Output       : 无
+ Return Value : 无
+*****************************************************************************/
+extern void Set_GPIO(unsigned char num);
+/*****************************************************************************
+ Prototype    : Clr_GPIO
+ Description  : GPIO端口置0
+ Input        : unsigned char num，GPIO端口号。
+ Output       : 无
+ Return Value : 无
+*****************************************************************************/
+extern void Clr_GPIO(unsigned char num);
+
+
+
+
+
 #endif /* PLATFORMNTO1_H_ */
